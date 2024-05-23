c26501181c5b155938c4043de0ee0d5ccf628bb1
theNightBirde <44029916+KINGSMITH97@users.noreply.github.com>
2024-05-23 08:02:56 +0000

    Create assignment2.2.sh

---
+++ assignment2.2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+username=$"Kings"
+
+#creating the directory
+mkdir $username
+
+cd $username
+
+#creating three files with different extensions
+touch index.html style.css script.js
+
+
+#listing the content in the directory
+ls -l
